21cdcd69a23c04de7b98bc8826446b2beeb88991
Jani <Janii.rognes@gmail.com>
2023-01-29 19:38:50 +0100

    made a list!

--- js/app.js
+++ js/app.js
@@ -1,21 +1,32 @@
 let shoppingList = [];
 
-function addItem(item) {
+const list = document.getElementById("list");
+const itemInput = document.getElementById("item");
+const addBtn = document.getElementById("add-btn");
+
+function addItem(e) {
+  e.preventDefault();
+  let item = itemInput.value;
   shoppingList.push(item);
-  console.log("Item added to the shopping list.");
+  renderList();
+  itemInput.value = "";
 }
 
-function deleteItem(item) {
-  let index = shoppingList.indexOf(item);
-  if (index > -1) {
-    shoppingList.splice(index, 1);
-    console.log("Item removed from the shopping list.");
-  } else {
-    console.log("Item not found in the shopping list.");
-  }
+//Delete Items
+
+function deleteItem(index) {
+  shoppingList.splice(index, 1);
+  renderList();
 }
 
-function showList() {
-  console.log("Shopping List:");
-  console.log(shoppingList.join(", "));
-} 
+//Create List
+function renderList() {
+  list.innerHTML = "";
+  shoppingList.forEach((item, index) => {
+    let li = document.createElement("li");
+    li.innerHTML = item + " <button onclick='deleteItem(" + index + ")'>Delete</button>";
+    list.appendChild(li);
+  });
+}
+
+addBtn.addEventListener("click", addItem);
